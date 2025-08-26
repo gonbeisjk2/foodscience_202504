acd3b84d3a882f1ba0c56b7320624cf127012673
taro shinjyuku <tarosjk@gmail.com>
2025-08-26 14:41:59 +0900

    TOPページWordPressループ部分（最新情報）完成

--- wp-content/themes/food-science/functions.php
+++ wp-content/themes/food-science/functions.php
@@ -6,3 +6,12 @@ add_action('wp_head', function () {
 
 // テーマサポート
 add_theme_support('title-tag'); //タイトルタグを適切なものにする
+add_theme_support('post-thumbnails'); //アイキャッチ画像を有効化する
+
+// titleタグの区切り文字を変更する
+add_filter('document_title_separator', 'my_document_title_separator');
+function my_document_title_separator($separator)
+{
+  $separator = '|';
+  return $separator;
+}
